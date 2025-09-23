221d438d3a1e0ef944601e64547a4823f42ebde2
Marcos  Castilho <marcosalmaa@icloud.com>
2025-09-23 23:39:44 +0800

    feat: practice on using unittest, modularisation, import statements, folder structure

--- week3/in-class-demos/workshop_part3/tests/test_greeter.py
+++ week3/in-class-demos/workshop_part3/tests/test_greeter.py
@@ -1,4 +1,10 @@
 # arrange
+import unittest
+from src.greetings import greet
+
+class TestGreeting(unittest.TestCase):
+    def test_greets_hello_world(self):
+        self.assertEqual("Hello, World", greet())
 
 # act
 
